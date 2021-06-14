208e0b15d642bf67170b60de12c5099ba5015627
Rian292 <rian.90@hotmail.com.br>
2021-06-13 23:20:54 -0300

    implment nav bar, header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+
+
+//Componentes
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './pokemon/template/header/header.component';
+import { FooterComponent } from './pokemon/template/footer/footer.component';
+import { NavComponent } from './pokemon/template/nav/nav.component';
+
+//Modulos
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
